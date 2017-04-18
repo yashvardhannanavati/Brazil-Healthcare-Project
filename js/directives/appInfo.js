5beb1e7d9dae3e2915948546f2e3faa05cbba1b1
yashvardhannanavati <yashn@bu.edu>
2017-04-17 22:04:24 -0400

    Login implemented

--- js/directives/appInfo.js
+++
@@ -1,9 +0,0 @@
-app.directive('app-info', function(){
-  return{
-    restrict: 'E',
-    scope: {
-      info: '='
-    },
- templateUrl: 'js/directives/appInfo.html'
-  };
-  });
--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ $(document).ready(function(){
 
 	$('input[type="text"]').val("");  // Reset all textfield value
 	
-	resetOptionValue();
 	initiateTabActivation();
 
 });
